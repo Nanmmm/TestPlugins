5fa545875c1d7f1e23550ea12c5de00815a551af
Nanmmm <89445455+Nanmmm@users.noreply.github.com>
2026-03-30 10:14:38 +0530

    Add initial build.gradle.kts configuration

---
+++ AllMoviesHub/build.gradle.kts
@@ -0,0 +1,9 @@
+version = 1
+
+cloudstream {
+    description = "Watch Movies from AllMoviesHub — Bollywood, Hollywood & South"
+    authors = listOf("Nanmmm")
+    status = 1
+    tvTypes = listOf("Movie")
+    iconUrl = "https://allmovieshub.golf/wp-content/uploads/2021/06/cropped-favicon-1-192x192.png"
+}
